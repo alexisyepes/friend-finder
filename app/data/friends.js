a43472dcbc781fad7e5b0cb9fecb52bd39c42aee
Alex Yepes <alexisyepes@yahoo.com>
2019-02-22 17:11:27 -0500

    all files modified to fix errors

---
+++ app/data/friends.js
@@ -0,0 +1,174 @@
+var friendsArray = [
+    {
+        name: "Antonio Banderas",
+        "photo": "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-img.instyle.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F684xflex%2Fpublic%2Fimages%2F2017%2F10%2F103117-antonio-banderas-lead.jpg%3Fitok%3DL7jLisyp&w=400&c=sc&poi=face&q=85",
+        "scores": [
+            2,
+            1,
+            4,
+            3,
+            5,
+            4,
+            4,
+            5,
+            2,
+            4
+        ]
+    },
+
+    {
+        name: "Homer Simpson",
+        "photo": "https://vignette.wikia.nocookie.net/simpsons/images/b/bd/Homer_Simpson.png/revision/latest?cb=20140126234206",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+
+    {
+        name: "Jennifer Aniston",
+        "photo": "https://www.wewatchtogether.com/wp-content/uploads/2018/12/jennifer-aniston-upcoming-movies-2017-2018-2019.jpg",
+        "scores": [
+            1,
+            1,
+            2,
+            3,
+            3,
+            2,
+            1,
+            1,
+            2,
+            3
+        ]
+    },
+
+    {
+        name: "Will Smith",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHRk5-zG8NCD9K83_NBOHf31VmvF1Zk4-guWoIcgPiXiS5C7Cw",
+        "scores": [
+            1,
+            5,
+            5,
+            5,
+            1,
+            5,
+            4,
+            4,
+            4,
+            2
+        ]
+    },
+
+    {
+        name: "Pedro Navaja",
+        "photo": "http://www.maestravida.com/Rubenfedorab.gif",
+        "scores": [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            4
+        ]
+    },
+
+    {
+        name: "John Smith",
+        "photo": "https://static1.fjcdn.com/comments/Gtbe+me+gtbe+14+gtget+random+_1fbde7f4b9a2dad3ee88dfa6fde96246.png",
+        "scores": [
+            3,
+            3,
+            3,
+            4,
+            3,
+            1,
+            3,
+            5,
+            3,
+            3
+        ]
+    },
+
+    {
+        name: "Andrea Echeverry",
+        "photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F8e%2F6b%2F20%2F8e6b20631e941aef20b2663d7caf1b06.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F267542034086743565%2F&docid=E-0p8dNiIl3KIM&tbnid=zNWn_ohd0dgzrM%3A&vet=10ahUKEwjnyeOD3M3gAhUaoYMKHa85BBcQMwg_KAkwCQ..i&w=200&h=240&safe=active&bih=752&biw=767&q=andrea%20echeverry&ved=0ahUKEwjnyeOD3M3gAhUaoYMKHa85BBcQMwg_KAkwCQ&iact=mrc&uact=8",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+
+    {
+        name: "Shakira",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdWEUaB9dbaznik1uFKrgkwycGwRXBSliyAKlnYQAudyi_FMZ3",
+        "scores": [
+            4,
+            5,
+            4,
+            3,
+            3,
+            2,
+            4,
+            1,
+            5,
+            5
+        ]
+    },
+
+    {
+        name: "Juan Pablo Angel",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRX4g1zsR99KG6vJYLP4hZk6LnsJhaTtCUjKIruNnA1yAoqepgj",
+        "scores": [
+            1,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            4,
+            5
+        ]
+    },
+
+    {
+        name: "Pablo Montoya",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL5orydPX9gc4ohrZjqlFGp88O7CFOEAH_hu0KTn5FFByI5NQqGA",
+        "scores": [
+            1,
+            1,
+            1,
+            2,
+            2,
+            2,
+            3,
+            3,
+            3,
+            1
+        ]
+    }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,46 @@
+var path = require('path');
+
+// Importing the list of friends
+var friends = require('../data/friends');
+
+// Exporting API routes
+module.exports = function(app) {
+
+	// Total list of friends
+	app.get('/api/friends', function(req, res) {
+		res.json(friends);
+	});
+
+	// Adding new friend entry
+	app.post('/api/friends', function(req, res) {
+
+		var userInput = req.body;
+
+		var userResponses = userInput.scores;
+
+		var matchName = '';
+		var matchImage = '';
+		var totalDifference = 10000;
+
+		for (var i = 0; i < friends.length; i++) {
+		
+			var diff = 0;
+			for (var j = 0; j < userResponses.length; j++) {
+				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+			}
+
+			if (diff < totalDifference) {
+				
+				totalDifference = diff;
+				matchName = friends[i].name;
+				matchImage = friends[i].photo;
+			}
+		}
+
+		// Adding new user 
+
+		friends.push(userInput);
+
+		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+	});
+};
